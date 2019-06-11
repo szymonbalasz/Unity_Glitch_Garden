7547bdca8562d9ba4f89e262de38fdb6dd705597
Szymon Balasz <sbalasz@gmail.com>
2019-06-11 22:23:54 +0200

    splash and start menu screens

---
+++ Glitch Garden/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] float LoadSceneDelay = 4f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("Splash Screen")) { StartCoroutine(LoadStartScreen()); }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator LoadStartScreen()
+    {
+        yield return new WaitForSeconds(LoadSceneDelay);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
